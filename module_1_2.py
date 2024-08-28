995d2e98dc1bfd5b4537efe87e6c6da2aef702d1
RaisaMahpirova <75176067+RaisaMahpirova@users.noreply.github.com>
2024-08-28 20:41:39 +0200

    Практическое задание переменные

---
+++ module_1_2.py
@@ -0,0 +1,6 @@
+amount_of_homeworks = 12
+amount_of_hours = 1.5
+name_of_course = 'Python'
+average_task_time = amount_of_hours / amount_of_homeworks
+print('Курс:', name_of_course, ', всего задач: ', amount_of_homeworks, ', затрачено часов: ', amount_of_hours,
+      ', среднее время выполнения ', average_task_time, ' часа.')
