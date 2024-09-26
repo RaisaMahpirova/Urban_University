73bb8590b918da16f4abf97b8051f98b227d5dcb
RaisaMahpirova <75176067+RaisaMahpirova@users.noreply.github.com>
2024-09-26 20:03:00 +0200

    Специальные методы классов

---
+++ module_5_2.py
@@ -0,0 +1,28 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors:
+            print("такого этажа нет")
+            return 0
+        for i in range(1, new_floor+1):
+            print(i)
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1.__len__())
+print(len(h2))
+
+print(h1)
+print(h2.__str__())
+# dunder-функции можно вызвать двумя способами
