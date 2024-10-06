d2e970a7738edef637554408e8457a8a6dd81a84
RaisaMahpirova <75176067+RaisaMahpirova@users.noreply.github.com>
2024-10-06 17:17:09 +0200

    Добавлены новые функции
    приветствие и проверка условий теперь реализуется с помощью функций, а не в основном коде

--- module_5_practice.py
+++ module_5_practice.py
@@ -28,10 +28,6 @@ class User:
         self.username = username
         is_confirm = False
         while not is_confirm:
-#            if len(password) < 8:
-#                print("Пароль должен содержать 8 знаков")
-#                password = input("Введите пароль: ")
-#                password_confirm = input("Повторите пароль: ")
             if password_confirm == password:
                 self.password = password
                 is_confirm = True
@@ -40,40 +36,53 @@ class User:
                 password = input("Введите пароль: ")
                 password_confirm = input("Повторите пароль: ")
 
+def registration():
+    global failed
+    name = input("Введите логин: ")
+    if name in database.data:
+        failed = True
+        print("Пользователь с таким логином уже существует. Выберите другой логин или войдите.")
+        return
+    else:
+        enter_password = input("Введите пароль: ")
+        while len(enter_password) < 8:
+            print("Пароль должен содержать минимум 8 символов")
+            enter_password = input("Введите пароль: ")
+        repeat_password = input("Повторите пароль: ")
+        user = User(name, enter_password, repeat_password)
+        failed = False
+        database.add_user(user.username, user.password)
+        print("Вы успешно зарегестрировались!")
+
+
+def log_in():
+    global failed
+    login = input("Введите логин: ")
+    if login in database.data:
+        password1 = input("Введите пароль: ")
+        if password1 == database.data[login]:
+            failed = False
+            print("Вход выполнен успешно!")
+        else:
+            database.verification(login, password1)
+    else:
+        failed = True
+        print("Пользователь с таким логином не найден! Зарегестрируйтесь или попробуйте снова.")
+
+
+def welcome():
+    if not failed:
+        print("Добро пожаловать!", end=' ')
+    choice = int(input("Выберите действие:\n1 - Вход\n2 - Регистрация\n"))
+    if choice == 1:
+        log_in()
+    if choice == 2:
+        registration()
+
 
 if __name__ == '__main__':
     database = Database()
     failed = False
     while True:
-        if not failed:
-            print("Добро пожаловать!", end=' ')
-        choice = int(input("Выберите действие:\n1 - Вход\n2 - Регистрация\n"))
-        if choice == 1:
-            login = input("Введите логин: ")
-            if login in database.data:
-                password1 = input("Введите пароль: ")
-                if password1 == database.data[login]:
-                    failed = False
-                    print("Вход выполнен успешно!")
-                else:
-                    database.verification(login, password1)
-            else:
-                failed = True
-                print("Пользователь с таким логином не найден! Зарегестрируйтесь или попробуйте снова.")
+        welcome()
 
-        if choice == 2:
-            name = input("Введите логин: ")
-            if name in database.data:
-                failed = True
-                print("Пользователь с таким логином уже существует. Выберите другой логин или войдите.")
-                continue
-            else:
-                enter_password = input("Введите пароль: ")
-                while len(enter_password) < 8:
-                    print("Пароль должен содержать минимум 8 символов")
-                    enter_password = input("Введите пароль: ")
-                repeat_password = input("Повторите пароль: ")
-                user = User(name, enter_password, repeat_password)
-                failed = False
-                database.add_user(user.username, user.password)
-                print("Вы успешно зарегестрировались!")
