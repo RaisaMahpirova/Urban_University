25684b007af47537493ce61759678bcfc7d8b7b7
RaisaMahpirova <75176067+RaisaMahpirova@users.noreply.github.com>
2024-12-26 00:56:02 +0200

    Try и Except

---
+++ module_8_1.py
@@ -0,0 +1,13 @@
+def add_everything_up(a, b):
+    try:
+        res = round(a + b, 3)
+    except TypeError:
+        res = str(a) + str(b)
+    finally:
+        return res
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
+
